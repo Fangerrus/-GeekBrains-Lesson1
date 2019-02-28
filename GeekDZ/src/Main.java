1a9a1fb97d4753a8c32e96fe6c72b70a53cc82be
Fangerrus <fangerrus@gmail.com>
2019-02-28 14:25:05 +0300

    Fix tasks 4,7,8.

--- GeekDZ/src/Main.java
+++ GeekDZ/src/Main.java
@@ -8,10 +8,10 @@ public class Main {
         System.out.println("\nЗадание 1: Создать переменные всех пройденных типов данных, и инициализировать их значения.");
         variables();
         System.out.print(expression(4,6,7,10));
-        System.out.print(task3(15, 2));
+        System.out.print(task3(10, 2));
         task4(0);
         System.out.print(task5(-1));
-        task6();
+        task6("Наставник");
         task7();
 
 
@@ -51,7 +51,7 @@ public class Main {
     public static boolean task3(int age1, int age2){
         System.out.println("\n" + "\n" + "Задание 3: Написать метод, принимающий на вход два числа, и проверяющий что их сумма лежит \nв пределах от 10 до 20(включительно), если да – вернуть true, в противном случае – false; ");
         System.out.print("\nСумма чисел = " + (age1 + age2) + " результат: ");
-        if (age1 + age2 > 10 && age1 + age2 < 20 ) {
+        if (age1 + age2 >= 10 && age1 + age2 <= 20) {
             return true;
 
         } else {
@@ -85,9 +85,8 @@ public class Main {
         }
     }
 
-    public static void task6 () {
+    public static void task6 (String name) {
         System.out.println("\n" + "\nЗадение 6: Написать метод, которому в качестве параметра передается строка, обозначающая имя, метод\nдолжен вывести в консоль сообщение «Привет, указанное_имя!»");
-        String name = "Vlad";
         System.out.print("\nПривет, " + name + "!");
 
     }
@@ -97,6 +96,7 @@ public class Main {
         System.out.print("\nВведите год: ");
         Scanner scanner = new Scanner(System.in);
         int year = scanner.nextInt();
+        scanner.close();
 
         if (((year % 4 == 0) && !(year % 100 == 0)) || (year % 400 == 0))
             System.out.println("Високосный год");
